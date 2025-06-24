42b33281cbc05d8b3ba8e8a7d43c2d3bacfcec12
Yoshi <ny000815@gmail.com>
2025-06-24 21:41:44 +0900

    Create 250617A-Spoiler.cpp

---
+++ Daily_Training/250617A-Spoiler.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+  string S;
+  vector<char> Answer;
+  cin >> S;
+  //cout << S;
+  bool inside = 0;
+  for(int i = 0; i < S.size(); i++){
+    if(S[i] == '|' && inside == 1) inside = 0;
+    else if(S[i] == '|' && inside == 0) inside = 1;
+    if(!inside && S[i] != '|') Answer.push_back(S[i]);
+  }
+  if(Answer.size() > 0){
+    for(int i = 0; i < Answer.size(); i++) cout << Answer[i];
+    cout << endl;
+  }
+  return 0;
+}
