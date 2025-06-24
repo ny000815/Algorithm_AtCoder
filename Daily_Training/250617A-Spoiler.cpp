985a71cf21e73bacc0e4bbafd2e7897291ce4d48
Yoshi <ny000815@gmail.com>
2025-06-24 21:54:20 +0900

    Create 250617A-Spoiler.cpp

--- Daily_Training/250617A-Spoiler.cpp
+++ Daily_Training/250617A-Spoiler.cpp
@@ -9,13 +9,13 @@ int main(){
   cin >> S;
   //cout << S;
   bool inside = 0;
-  for(int i = 0; i < S.size(); i++){
+  for(auto i = 0; i < S.size(); i++){
     if(S[i] == '|' && inside == 1) inside = 0;
     else if(S[i] == '|' && inside == 0) inside = 1;
     if(!inside && S[i] != '|') Answer.push_back(S[i]);
   }
   if(Answer.size() > 0){
-    for(int i = 0; i < Answer.size(); i++) cout << Answer[i];
+    for(auto i = 0; i < Answer.size(); i++) cout << Answer[i];
     cout << endl;
   }
   return 0;
